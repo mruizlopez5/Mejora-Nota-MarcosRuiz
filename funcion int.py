5124cc1fdd82cc8b4d8818fb6c4de0ce1abbee92
Mark.os <mruizlopez5@educacion.navarra.es>
2024-11-20 23:31:17 +0100

    Create funcion int.py

---
+++ funcion int.py
@@ -0,0 +1,46 @@
+while True:
+
+
+    cadena = input("introduce numeros")
+
+    lista = list(cadena)
+    
+    
+    N = len(lista)
+    total = 0
+    lista.reverse()
+
+    des = True
+
+    for i in range(N):
+        if ":" > lista[i] > "/" or "," < lista[i] < ".":
+            a = 1
+        else:  
+            des = False
+
+    if des != False:
+
+
+        for i in range(N):
+                    if lista[i] == "1":
+                        total = total + 1*pow(10, i)
+                    elif lista[i] == "2":
+                        total = total + 2*pow(10, i)
+                    elif lista[i] == "3":
+                        total = total + 3*pow(10, i)
+                    elif lista[i] == "4":
+                        total = total + 4*pow(10, i)
+                    elif lista[i] == "5":
+                        total = total + 5*pow(10, i)
+                    elif lista[i] == "6":
+                        total = total + 6*pow(10, i)
+                    elif lista[i] == "7":
+                        total = total + 7*pow(10, i)
+                    elif lista[i] == "8":
+                        total = total + 8*pow(10, i)
+                    elif lista[i] == "9":
+                        total = total + 9*pow(10, i)
+                    elif lista[i] == "-":
+                        total= total*-1
+        print(total)
+    else: print("malplan")
